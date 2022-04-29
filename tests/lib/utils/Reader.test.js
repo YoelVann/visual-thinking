153646220e86b5c450c988e911c0cf00abffeb35
Joel Hoil <yoelvann@gmail.com>
2022-04-29 17:48:22 -0500

    Refactoring readJsonFile with tests passed

--- tests/lib/utils/Reader.test.js
+++ tests/lib/utils/Reader.test.js
@@ -8,4 +8,13 @@ describe("Tests in Reader class", () => {
 
         expect(typeof jsonFile).toBe("object");
     });
+
+    test("2. readJSONFile() must to return an error object if the param is undefined or null", () => { 
+        
+        const jsonFile = Reader.readJsonFile();
+        const jsonFile2 = Reader.readJsonFile(undefined);
+
+        expect(jsonFile.error).toBe("FATAL: JSON file is required");
+        expect(jsonFile2.error).toBe("FATAL: JSON file is required");
+    });
 });
