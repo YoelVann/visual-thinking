99e8f9e8bf49ad1dd5890f653976bea87172c631
Joel Hoil <yoelvann@gmail.com>
2022-04-30 17:03:53 -0500

    TDD-Test in StudentsController in Method getAllStudents

---
+++ tests/lib/controllers/StudentsController.test.js
@@ -0,0 +1,18 @@
+const StudentsController = require("../../../lib/controllers/StudentsController");
+const Reader = require("../../../lib/utils/Reader");
+
+describe("Tests in StudentsController class", () => {
+
+    test("1. static method getAllStudents must return all students given a students json file", () => { 
+
+        const studentsJson = Reader.readJsonFile("students.json");
+        const studentsJsonId = studentsJson.map(student => student.id);
+
+        const studentsReturned = StudentsController.getAllStudents();
+        const studentsReturnedId = studentsReturned.map(student => student.id);
+
+        const verifyAllStudentsReturned = studentsReturnedId.every(studentId => studentsJsonId.includes(studentId))
+        
+        expect(verifyAllStudentsReturned).toBeTruthy();
+    });
+});
